70fa1605407ff41bd29a10f7950617347ad6a77d
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-02 01:05:57 +0700

    Placeholder
    Trying to make Activity class with small foot-print

---
+++ src/com/dimasdanz/keamananpintu/usermodel/UserAsyncTask.java
@@ -0,0 +1,18 @@
+package com.dimasdanz.keamananpintu.usermodel;
+
+import java.util.List;
+
+import android.os.AsyncTask;
+
+public class UserAsyncTask extends AsyncTask<List<UserModel>, Void, Boolean>{
+	
+	public UserAsyncTask() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected Boolean doInBackground(List<UserModel>... params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
