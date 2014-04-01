88c417ab511f63142552ca20698fd3fa64cad5de
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-02 01:08:19 +0700

    Add User Model to handle User Activity

--- src/com/dimasdanz/keamananpintu/usermodel/UserModel.java
+++ src/com/dimasdanz/keamananpintu/usermodel/UserModel.java
@@ -3,10 +3,12 @@ package com.dimasdanz.keamananpintu.usermodel;
 public class UserModel {
 	private String user_name = null;
     private String user_id = null;
+    private String user_password = null;
 
-    public UserModel(String userid, String username) {
+    public UserModel(String userid, String username, String password) {
     	this.user_id = userid;
         this.user_name = username;
+        this.user_password = password;
     }
     
     public String getUserID() {
@@ -15,5 +17,9 @@ public class UserModel {
 
     public String getName() {
 	    return user_name;
+    }
+    
+    public String getPassword() {
+    	return user_password;
     }
 }
