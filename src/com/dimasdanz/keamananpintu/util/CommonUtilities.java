106514128082d889000f176aaa0eeecf48c617ad
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-02 01:07:06 +0700

    Add new Util

--- src/com/dimasdanz/keamananpintu/util/CommonUtilities.java
+++ src/com/dimasdanz/keamananpintu/util/CommonUtilities.java
@@ -1,8 +1,14 @@
 package com.dimasdanz.keamananpintu.util;
 
+import com.dimasdanz.keamananpintu.R;
+
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
 public final class CommonUtilities {
+
 	private CommonUtilities() {}
 	
 	public static String deviceStatusUrl(Context context){
@@ -31,5 +37,32 @@ public final class CommonUtilities {
 	
 	public static String getUserList(Context context){
 		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_get_users/";
+	}
+	
+	public static String getInsertUser(Context context){
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_insert_user";
+	}
+	
+	public static String getUpdateUser(Context context){
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_update_user";
+	}
+	
+	public static String getDeleteUser(Context context){
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_delete_user";
+	}
+	
+	public static void dialogConnectionError(final Context context){
+		AlertDialog.Builder builder = new AlertDialog.Builder(context, 4);
+		builder.setIcon(android.R.drawable.ic_dialog_info);
+    	builder.setTitle(R.string.title_dialog_server_error);
+    	builder.setMessage(R.string.message_dialog_connection_error);
+    	builder.setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
+        	public void onClick(DialogInterface dialog, int id) {
+        		Activity a = (Activity) context;
+        		a.finish();
+        	}
+        });
+    	AlertDialog alert = builder.create();
+    	alert.show();
 	}
 }
