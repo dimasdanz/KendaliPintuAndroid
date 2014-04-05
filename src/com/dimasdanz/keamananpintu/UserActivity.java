efaa45169e086542d6859f8d2d3e29a09353570d
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-05 23:18:02 +0700

    Implement send data asynctask class

--- src/com/dimasdanz/keamananpintu/UserActivity.java
+++ src/com/dimasdanz/keamananpintu/UserActivity.java
@@ -7,11 +7,14 @@ import com.dimasdanz.keamananpintu.usermodel.UserAdapter;
 import com.dimasdanz.keamananpintu.usermodel.UserDialogManager.UserDialogManagerListener;
 import com.dimasdanz.keamananpintu.usermodel.UserLoadData;
 import com.dimasdanz.keamananpintu.usermodel.UserLoadData.UserLoadDataListener;
+import com.dimasdanz.keamananpintu.usermodel.UserSendData.UserSendDataListener;
 import com.dimasdanz.keamananpintu.usermodel.UserDialogManager;
 import com.dimasdanz.keamananpintu.usermodel.UserListView;
 import com.dimasdanz.keamananpintu.usermodel.UserModel;
+import com.dimasdanz.keamananpintu.usermodel.UserSendData;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,7 +24,7 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 
-public class UserActivity extends FragmentActivity implements UserDialogManagerListener, UserListView.EndlessListener, OnItemClickListener, UserLoadDataListener{
+public class UserActivity extends FragmentActivity implements UserDialogManagerListener, UserListView.EndlessListener, OnItemClickListener, UserLoadDataListener, UserSendDataListener{
 	UserListView userLv;
 	UserAdapter userAdp;
 	Integer page = 1;
@@ -41,7 +44,6 @@ public class UserActivity extends FragmentActivity implements UserDialogManagerL
 		userLv.setOnItemClickListener(this);
 		
 		userLv.setLoadingView(R.layout.loading_layout);
-		userLv.setLoadedView(R.layout.loaded_layout);
 		userLv.setAdapter(userAdp);
 		
 		userLv.setListener(UserActivity.this);
@@ -87,15 +89,13 @@ public class UserActivity extends FragmentActivity implements UserDialogManagerL
 	}
 
 	@Override
-	public void onDialogPositiveClick(DialogFragment dialog,ArrayList<String> data) {
-		// TODO Submit data to server
-		
+	public void onDialogPositiveClick(DialogFragment dialog, ArrayList<String> data) {
+		new UserSendData(this).execute(data.get(0), data.get(1), data.get(2));
 	}
-
+	
 	@Override
-	public void onDialogNegativeClick(DialogFragment dialog) {
-		// TODO Delete data maybe?
-		
+	public void onDialogNegativeClick(DialogFragment dialog, ArrayList<String> data) {
+		new UserSendData(this).execute("delete", data.get(0));
 	}
 
 	@Override
@@ -114,6 +114,18 @@ public class UserActivity extends FragmentActivity implements UserDialogManagerL
 			page += 1;
 		}else{
 			userLv.addNewData(null);
+		}
+	}
+
+	@Override
+	public void onSendDataComplete(Boolean result) {
+		Log.d("Result", result.toString());
+		if(result){
+			userAdp.clear();
+			page = 1;
+			new UserLoadData(this, page).execute();
+		}else{
+			
 		}
 	}
 }
