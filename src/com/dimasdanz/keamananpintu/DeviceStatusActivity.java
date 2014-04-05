02abf37cf6ddeceadca6657971fb9c5113be9d92
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-05 14:55:44 +0700

    Implement separated asynctask class

--- src/com/dimasdanz/keamananpintu/DeviceStatusActivity.java
+++ src/com/dimasdanz/keamananpintu/DeviceStatusActivity.java
@@ -1,19 +1,14 @@
 package com.dimasdanz.keamananpintu;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import com.dimasdanz.keamananpintu.devicestatusmodel.DeviceStatusModel;
+import com.dimasdanz.keamananpintu.devicestatusmodel.DeviceStatusLoadData;
+import com.dimasdanz.keamananpintu.devicestatusmodel.DeviceStatusLoadData.DeviceStatusLoadDataListener;
+import com.dimasdanz.keamananpintu.devicestatusmodel.DeviceStatusSendData;
+import com.dimasdanz.keamananpintu.devicestatusmodel.DeviceStatusSendData.DeviceStatusSendDataListener;
 import com.dimasdanz.keamananpintu.util.CommonUtilities;
-import com.dimasdanz.keamananpintu.util.JSONParser;
 import com.dimasdanz.keamananpintu.util.UniversalDialogManager;
 import com.dimasdanz.keamananpintu.util.UniversalDialogManager.DialogManagerListener;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,14 +22,19 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 
-public class DeviceStatusActivity extends FragmentActivity implements DialogManagerListener, OnCheckedChangeListener {
+public class DeviceStatusActivity extends FragmentActivity implements DialogManagerListener, OnCheckedChangeListener, DeviceStatusLoadDataListener, DeviceStatusSendDataListener {
+	private EditText textAttempts;
+	private Switch statusSwitch;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_device_status);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		textAttempts = (EditText)findViewById(R.id.txtAttempts);
+		statusSwitch = (Switch)findViewById(R.id.swStatus);
 		
-		new GetDeviceStatus().execute();
+		new DeviceStatusLoadData(this).execute();
 	}
 
 	@Override
@@ -55,163 +55,104 @@ public class DeviceStatusActivity extends FragmentActivity implements DialogMana
 	
 	@Override
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		new sendDatatoServer(0).execute(Boolean.toString(isChecked));	
+		new DeviceStatusSendData(this, 0).execute(String.valueOf(isChecked));
 	}
 	
 	public void onClickButtonUnlock(View v){
-		new sendDatatoServer(2).execute();
+		new DeviceStatusSendData(this, 2).execute();
 	}
 	
 	public void onClickAttemptsSetting(View v){
-		EditText pa = (EditText)findViewById(R.id.txtAttempts);
 		new UniversalDialogManager();
-		DialogFragment dialogInputAttempts = UniversalDialogManager.newInstance(1, pa.getText().toString());
+		DialogFragment dialogInputAttempts = UniversalDialogManager.newInstance(1, textAttempts.getText().toString());
 		dialogInputAttempts.show(getSupportFragmentManager(), "InputPasswordAttempts");
 	}
 
 	@Override
 	public void onDialogPositiveClick(DialogFragment dialog, String data) {
-		new sendDatatoServer(1).execute(data);
-		EditText pa = (EditText)findViewById(R.id.txtAttempts);
-		pa.setText(data);
+		new DeviceStatusSendData(this, 1).execute(data);
+		textAttempts.setText(data);
 	}
 	
-	class GetDeviceStatus extends AsyncTask<Void, Void, Boolean> {
-		boolean condition;
-		boolean status;
-		String password_attempts;
-		JSONParser jsonParser = new JSONParser();
+	@Override
+	public void onLoadDataProgress() {
+		setView(0);
+	}
 
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
+	@Override
+	public void onLoadDataComplete(DeviceStatusModel results) {
+		if(results != null){
+			if(results.getCondition()){
+				setView(1);
+			}else{
+				setView(2);
+				textAttempts.setText(String.valueOf(results.getPasswordAttempts()));
+				statusSwitch.setChecked(results.getStatus());
+				statusSwitch.setOnCheckedChangeListener(this);
+			}
+		}else{
+			CommonUtilities.dialogConnectionError(this);
+		}
+	}
+
+	@Override
+	public void onSendDataComplete(int result) {
+		switch (result) {
+		case 0:
+			Toast.makeText(this, R.string.toast_device_locked, Toast.LENGTH_SHORT).show();
+			new DeviceStatusLoadData(this).execute();
+			break;
+		case 1:
+			Toast.makeText(this, R.string.toast_device_status_on, Toast.LENGTH_SHORT).show();
+			break;
+		case 2:
+			Toast.makeText(this, R.string.toast_device_status_off, Toast.LENGTH_SHORT).show();
+			break;
+		case 3:
+			Toast.makeText(this, R.string.toast_device_attempts_change, Toast.LENGTH_SHORT).show();
+			break;
+		case 4:
+			Toast.makeText(this, R.string.toast_device_unlocked, Toast.LENGTH_SHORT).show();
+			new DeviceStatusLoadData(this).execute();
+			break;
+		case 5:
+			Toast.makeText(this, R.string.toast_connection_error, Toast.LENGTH_SHORT).show();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void setView(int type){
+		switch (type) {
+		case 0:
 			findViewById(R.id.layout_progress_bar).setVisibility(View.VISIBLE);
 			findViewById(R.id.layout_device_locked).setVisibility(View.GONE);
 			findViewById(R.id.layout_condition).setVisibility(View.GONE);
 			findViewById(R.id.layout_attempts).setVisibility(View.GONE);
 			findViewById(R.id.layout_status).setVisibility(View.GONE);
-		}
-
-		protected Boolean doInBackground(Void... args) {
-			List<NameValuePair> params = new ArrayList<NameValuePair>();
-			JSONObject json = jsonParser.makeHttpRequest(CommonUtilities.deviceStatusUrl(getApplicationContext()),"GET", params);
-			if(json != null){
-				try {
-					password_attempts = json.getString("password_attempts");
-					condition = json.getBoolean("condition");
-					status = json.getBoolean("status");
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-				return true;
-			}else{
-				return false;
-			}
-		}
-
-		protected void onPostExecute(Boolean result) {
+			break;
+		case 1:
 			findViewById(R.id.layout_progress_bar).setVisibility(View.GONE);
-			if(result){
-				if(condition){
-					findViewById(R.id.layout_device_locked).setVisibility(View.VISIBLE);
-					findViewById(R.id.layout_condition).setVisibility(View.GONE);
-					findViewById(R.id.layout_attempts).setVisibility(View.GONE);
-					findViewById(R.id.layout_status).setVisibility(View.GONE);
-				}else{
-					findViewById(R.id.layout_device_locked).setVisibility(View.GONE);
-					findViewById(R.id.layout_condition).setVisibility(View.VISIBLE);
-					findViewById(R.id.layout_attempts).setVisibility(View.VISIBLE);
-					findViewById(R.id.layout_status).setVisibility(View.VISIBLE);
-					
-					EditText textAttempts = (EditText)findViewById(R.id.txtAttempts);
-					textAttempts.setText(password_attempts);
-					
-					Switch statusSwitch = (Switch)findViewById(R.id.swStatus);
-					statusSwitch.setChecked(status);
-					statusSwitch.setOnCheckedChangeListener(DeviceStatusActivity.this);
-				}
-			}else{
-				CommonUtilities.dialogConnectionError(DeviceStatusActivity.this);
-			}
-		}
-	}
-
-	
-	class sendDatatoServer extends AsyncTask<String, String, Integer> {
-		private int num;
-		JSONParser jsonParser = new JSONParser();
-		
-		protected sendDatatoServer(Integer i){
-			this.num = i;
-		}
-		
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-		}
-		
-		protected Integer doInBackground(String... args) {
-			JSONObject json = null;
-			List<NameValuePair> params = new ArrayList<NameValuePair>();
-			switch (num) {
-			case 0:
-				params.add(new BasicNameValuePair("status", args[0]));
-				json = jsonParser.makeHttpRequest(CommonUtilities.changeDeviceStatus(getApplicationContext()), "POST", params);
-				break;
-			case 1:
-				params.add(new BasicNameValuePair("password_attempts", args[0]));
-				json = jsonParser.makeHttpRequest(CommonUtilities.changeDeviceAttempts(getApplicationContext()), "POST", params);
-				break;
-			case 2:
-				json = jsonParser.makeHttpRequest(CommonUtilities.unlockDevice(getApplicationContext()), "POST", params);
-				break;
-			default:
-				break;
-			}
-			
-			if(json != null){
-				try {
-					return json.getInt("response");
-				} catch (JSONException e) {
-					return 5;
-				}
-			}else{
-				return 5;
-			}			
-		}
-		
-		/* Response Cheat Sheet
-		 * Response = 0 -> Device Locked
-		 * Response = 1 -> Change Device Status ON
-		 * Response = 2 -> Change Device Status OFF
-		 * Response = 3 -> Change Password Attempts
-		 * Response = 4 -> Device Unlocked 
-		 */
-		protected void onPostExecute(Integer result) {
-			switch (result) {
-			case 0:
-				Toast.makeText(DeviceStatusActivity.this, R.string.toast_device_locked, Toast.LENGTH_SHORT).show();
-				new GetDeviceStatus().execute();
-				break;
-			case 1:
-				Toast.makeText(DeviceStatusActivity.this, R.string.toast_device_status_on, Toast.LENGTH_SHORT).show();
-				break;
-			case 2:
-				Toast.makeText(DeviceStatusActivity.this, R.string.toast_device_status_off, Toast.LENGTH_SHORT).show();
-				break;
-			case 3:
-				Toast.makeText(DeviceStatusActivity.this, R.string.toast_device_attempts_change, Toast.LENGTH_SHORT).show();
-				break;
-			case 4:
-				Toast.makeText(DeviceStatusActivity.this, R.string.toast_device_unlocked, Toast.LENGTH_SHORT).show();
-				new GetDeviceStatus().execute();
-				break;
-			case 5:
-				Toast.makeText(DeviceStatusActivity.this, R.string.toast_connection_error, Toast.LENGTH_SHORT).show();
-				break;
-			default:
-				break;
-			}
+			findViewById(R.id.layout_device_locked).setVisibility(View.VISIBLE);
+			findViewById(R.id.layout_condition).setVisibility(View.GONE);
+			findViewById(R.id.layout_attempts).setVisibility(View.GONE);
+			findViewById(R.id.layout_status).setVisibility(View.GONE);
+			break;
+		case 2:
+			findViewById(R.id.layout_progress_bar).setVisibility(View.GONE);
+			findViewById(R.id.layout_device_locked).setVisibility(View.GONE);
+			findViewById(R.id.layout_condition).setVisibility(View.VISIBLE);
+			findViewById(R.id.layout_attempts).setVisibility(View.VISIBLE);
+			findViewById(R.id.layout_status).setVisibility(View.VISIBLE);
+			break;
+		default:
+			findViewById(R.id.layout_progress_bar).setVisibility(View.GONE);
+			findViewById(R.id.layout_device_locked).setVisibility(View.GONE);
+			findViewById(R.id.layout_condition).setVisibility(View.GONE);
+			findViewById(R.id.layout_attempts).setVisibility(View.GONE);
+			findViewById(R.id.layout_status).setVisibility(View.GONE);
+			break;
 		}
 	}
 }
