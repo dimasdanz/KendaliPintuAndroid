f0e27e8f0e86fd4a2976317ae1437e4952048af6
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-05 14:55:05 +0700

    Add new model for DeviceStatus

---
+++ src/com/dimasdanz/keamananpintu/devicestatusmodel/DeviceStatusModel.java
@@ -0,0 +1,25 @@
+package com.dimasdanz.keamananpintu.devicestatusmodel;
+
+public class DeviceStatusModel {
+	private int password_attempts;
+	private boolean status;
+	private boolean condition;
+	
+	public DeviceStatusModel(boolean status, int password_attempts, boolean condition) {
+		this.password_attempts = password_attempts;
+		this.status = status;
+		this.condition = condition;
+	}
+	
+	public int getPasswordAttempts(){
+		return password_attempts;
+	}
+	
+	public boolean getStatus(){
+		return status;
+	}
+	
+	public boolean getCondition(){
+		return condition;
+	}
+}
