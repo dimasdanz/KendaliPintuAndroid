339c8b73c98d84b58bc37c10b87bc92ef72728ce
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-24 00:57:24 +0700

    Change url to a new web API

--- src/com/dimasdanz/kendalipintu/util/ServerUtilities.java
+++ src/com/dimasdanz/kendalipintu/util/ServerUtilities.java
@@ -5,59 +5,59 @@ import android.content.Context;
 public class ServerUtilities {
 
 	public static String getLoginUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_login";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/login";
 	}
 	
 	public static String getAdminLoginUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_login_admin";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/login_admin";
 	}
 	
 	public static String getRegisterDeviceUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_register_device";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/register_device";
 	}
 	
 	public static String getOpenDoorUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_open_door";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/open_door";
 	}
 	
-	public static String deviceStatusUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_status";
-	}
-
-	public static String changeDeviceStatusUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_change_status";
-	}
-
-	public static String changeDeviceAttemptsUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_change_attempts";
-	}
-
-	public static String unlockDeviceUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_unlock";
-	}
-
 	public static String getLogDateUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_get_log_date";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/get_log_date";
 	}
 
 	public static String getLogDetailUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_get_detail_log";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/get_log_detail";
 	}
 
 	public static String getUserListUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_get_users/";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/get_users/";
 	}
 
 	public static String getInsertUserUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_insert_user";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/insert_user";
 	}
 
 	public static String getUpdateUserUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_update_user";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/update_user";
 	}
 
 	public static String getDeleteUserUrl(Context context){
-		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/dcs/dcs_delete_user";
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/delete_user";
+	}
+	
+	/* Deprecated */
+	public static String deviceStatusUrl(Context context){
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/status";
 	}
 
+	public static String changeDeviceStatusUrl(Context context){
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/change_status";
+	}
+
+	public static String changeDeviceAttemptsUrl(Context context){
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/change_attempts";
+	}
+
+	public static String unlockDeviceUrl(Context context){
+		return SharedPreferencesManager.getHostnamePrefs(context)+"/api/android/unlock";
+	}
 }
