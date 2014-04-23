8a284a626310038b1954fe1f411287a7c7ec9fbc
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-24 03:27:29 +0700

    New class to handle static string

---
+++ src/com/dimasdanz/kendalipintu/util/StaticString.java
@@ -0,0 +1,15 @@
+package com.dimasdanz.kendalipintu.util;
+
+public class StaticString {
+	public static String TAG_NO_ACCOUNT = "NO_ACCOUNT";
+	public static String TAG_INCORRECT_PASSWORD = "INCORRECT_PASSWORD";
+	public static String TAG_ENTER_NFC = "dimasdanz.kendalipintu.tag_masuk";
+	public static String TAG_EXIT_NFC = "dimasdanz.kendalipintu.tag_keluar";
+	public static String TAG_INVALID_NFC = "invalid_tag";
+	public static String TAG_SERVER_OFFLINE = "server_offline";
+	public static String TAG_ARDUINO_OFFLINE = "arduino_offline";
+	public static String TAG_STATUS_OFFLINE = "offline";
+	public static String TAG_STATUS_ACTIVE = "active";
+	public static String TAG_STATUS_INACTIVE = "inactive";
+
+}
